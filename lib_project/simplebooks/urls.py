5c3bf56829e6190d2bf24db03111333ca96647c8
noname2048 <sungwook.csw@gmail.com>
2021-08-12 19:52:17 +0900

    feat: delete deprecicated urls

--- lib_project/simplebooks/urls.py
+++ lib_project/simplebooks/urls.py
@@ -7,11 +7,9 @@ from rest_framework.routers import DefaultRouter
 app_name = "simplebooks"
 
 router = DefaultRouter()
-# router.register("bookshelf/recent/", views.RecentBookshelfListView.as_view(), basename="recent")
 router.register("bookshelf", views.BookshelfViewSet, basename="bookshelf")
 router.register("bookshelf/(?P<bookshelf_pk>[0-9]+)/book", views.BookViewSet, basename="book")
 
 urlpatterns = [
-    # path("bookshelf/recent/", views.RecentBookshelfListView.as_view(), name="recent"),
     path("", include(router.urls)),
 ]
